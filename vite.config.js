70193cf27d9a0bb725e789d5e360918a4a2f2dd0
Oleksandr Boitsun <thewip3out@gmail.com>
2021-11-09 17:56:38 +0200

    check htaccess

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
-const APP_ENV = process.env.APP_ENV || "";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `https://dino-portal.vercel.app/`,
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
