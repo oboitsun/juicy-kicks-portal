fe17a2240a5f9e3fbe659f677107b8050c7f0057
Oleksandr Boitsun <thewip3out@gmail.com>
2021-11-09 17:50:47 +0200

    check

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import react from "@vitejs/plugin-react";
 const APP_ENV = process.env.APP_ENV || "";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:
-    APP_ENV === "local"
-      ? `http://localhost:3000/dist/`
-      : `https://dino-portal.vercel.app/dist/`,
+  base: `https://dino-portal.vercel.app/`,
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
