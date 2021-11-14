574e669de28bf37fb2d6a645f56871f89c98501e
Oleksandr Boitsun <thewip3out@gmail.com>
2021-11-14 14:13:03 +0200

    added some animations to navigation tiles

--- src/components/HomeDashboard/ContactUs.jsx
+++ src/components/HomeDashboard/ContactUs.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 export default function ContactUs() {
   return (
-    <div className="nav-grid-item">
+    <div className="nav-grid-item contact">
       <div className="flex flex-col gap-5">
-        <img className="w-full " src="assets/faq.png" alt="faq" />
+        <div className="faq relative w-full h-full flex-grow">
+          <img className="w-full " src="assets/faq.png" alt="faq" />
+          <p className="text-lime text-7xl font-bold abs-centering">?</p>
+        </div>
         <button className="nav-grid-item-button contact-us">contact us</button>
       </div>
     </div>
--- src/components/HomeDashboard/Marketplace.jsx
+++ src/components/HomeDashboard/Marketplace.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { slides } from "../../pages/Marketplace/db";
+import "./marketplace.scss";
 export default function Marketplace() {
   return (
     <div className="nav-grid-item">
       <div className="flex flex-col gap-5 justify-between h-full">
-        <div className="w-full h-full">
-          <img
-            className="h-full object-cover"
-            src="assets/marketplace.png"
-            alt="marketplace"
-          />
+        <div className="w-full h-full relative overflow-hidden">
+          <div className="   market-grid">
+            {slides.map((card, i) => (
+              <div key={i} className="w-full h-auto">
+                <img className="w-full h-auto" src={card.imgSrc} alt={card.character} />
+              </div>
+            ))}
+          </div>
         </div>
         <Link to="/marketplace">
           <button className="nav-grid-item-button marketplace">marketplace</button>
