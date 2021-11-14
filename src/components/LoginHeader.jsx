2a3f02dbae6065aa93fe0de0a6889d118e328bf6
Oleksandr Boitsun <thewip3out@gmail.com>
2021-11-14 13:46:50 +0200

    back to landing page link added

--- src/components/LoginHeader.jsx
+++ src/components/LoginHeader.jsx
@@ -6,7 +6,7 @@ import Socials from "./Socials";
 export default function LoginHeader() {
   const dispatch = useDispatch();
   const links = [
-    { text: "about us", href: "/" },
+    { text: "about us", href: "https://juicy-kicks.vercel.app" },
     {
       text: "contact us",
       onClick: () => {
@@ -17,11 +17,17 @@ export default function LoginHeader() {
   return (
     <div className="max-w-[1080px] mx-auto flex items-center justify-between relative">
       <div className="flex gap-10">
-        {links.map((link) => (
-          <button key={link.text} className="text-white uppercase text-xl" {...link}>
-            {link.text}
-          </button>
-        ))}
+        {links.map((link) =>
+          link.text === "about us" ? (
+            <a className="text-white uppercase text-xl" href={link.href}>
+              {link.text}
+            </a>
+          ) : (
+            <button key={link.text} className="text-white uppercase text-xl" {...link}>
+              {link.text}
+            </button>
+          )
+        )}
       </div>
       <img
         className="w-auto top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 absolute"
